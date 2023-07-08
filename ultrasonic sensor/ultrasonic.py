35ea6a22f7f010f64306f32dd092e1f4175e3e56
AfrizaDicky <138385001+AfrizaDicky@users.noreply.github.com>
2023-07-08 14:49:51 +0700

    Update ultrasonic.py

--- ultrasonic sensor/ultrasonic.py
+++ ultrasonic sensor/ultrasonic.py
@@ -19,25 +19,23 @@ while True:
     print("tunggu sebentar!")
     time.sleep(0.2)
     GPIO.output(TRIG,True)
-    time.sleep(0.1)
+    time.sleep(0.0001)
     GPIO.output(TRIG, False)
-    while GPIO.input(ECHO)==0:
-        start=time.time()
-    while GPIO.input(ECHO)==1:
-        end=time.time()
-    duration=end-start
+    while GPIO.input(ECHO) == 0:
+        start= time.time()
+    while GPIO.input(ECHO) == 1:
+        end= time.time()
+    duration = end - start
     jarak= duration * 17150
     jarak= round(jarak,2)
     print("jaraknya adalah:", jarak, "cm")
     time.sleep(2)
-    
-#     pp= float(jarak)
-#     if pp >= 5.00:
-#         led1.on()
-#         time.sleep(1)
-#         led1.ff()
-#     else:
-#         led2.on()
-#         time.sleep(1)
-#         led2.off
+
+    if jarak <= 5 :
+        led1.on()
+        led2.on()
+
+    else:
+        led1.off()
+        led2.off(0
     
