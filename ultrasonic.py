01cae99abe2adea7f0cb309c14733ebe9de0b122
AfrizaDicky <138385001+AfrizaDicky@users.noreply.github.com>
2023-07-11 18:14:01 +0700

    Update ultrasonic.py

--- ultrasonic.py
+++ ultrasonic.py
@@ -11,33 +11,33 @@ TRIG = 26
 GPIO.setmode(GPIO.BCM)
 "GPIO.setwarnings(False)"
 
-
-while True:
-    GPIO.setup(ECHO, GPIO.IN)
-    GPIO.setup(TRIG, GPIO.OUT)
-    GPIO.output(TRIG, False)
-    print("tunggu sebentar!")
-    time.sleep(0.2)
-    GPIO.output(TRIG,True)
-    time.sleep(0.1)
-    GPIO.output(TRIG, False)
-    while GPIO.input(ECHO)==0:
-        start=time.time()
-    while GPIO.input(ECHO)==1:
-        end=time.time()
-    duration=end-start
-    jarak= duration * 17150
-    jarak= round(jarak,2)
-    print("jaraknya adalah:", jarak, "cm")
-    time.sleep(2)
+try:
+    while True:
+        GPIO.setup(ECHO, GPIO.IN)
+        GPIO.setup(TRIG, GPIO.OUT)
+        GPIO.output(TRIG, False)
+        print("tunggu sebentar!")
+        time.sleep(0.2)
+        GPIO.output(TRIG,True)
+        time.sleep(0.1)
+        GPIO.output(TRIG, False)
+        while GPIO.input(ECHO)==0:
+            start=time.time()
+        while GPIO.input(ECHO)==1:
+            end=time.time()
+        duration=end-start
+        jarak= duration * 17150
+        jarak= round(jarak,2)
+        print("jaraknya adalah:", jarak, "cm")
+        time.sleep(2)
     
-    if jarak >= 6 :
-        led1.on()
-        led2.on()
+        if jarak >= 6 :
+            led1.on()
+            led2.on()
         
-    else:
-        led1.off()
-        led2.off()
-        
-GPIO.cleanup()
+        else:
+            led1.off()
+            led2.off()
+except KeyboardInterrupt:        
+    GPIO.cleanup()
     
